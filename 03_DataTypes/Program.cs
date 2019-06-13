a1c01c203e50e92b8838b278929a857096374181
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-13 23:58:18 +0300

    Add files via upload

---
+++ 03_DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nihil";
+            char nihil = 'N';
+            int number = 12;
+            double pi = 3.14;
+            bool isNihil = true;
+
+            Console.WriteLine("Basics of data types!");
+            Console.ReadKey();
+        }
+    }
+}
