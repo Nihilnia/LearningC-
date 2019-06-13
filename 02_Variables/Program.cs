18976c06d0fea123dbf79413250fdd35c56acbeb
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-13 22:33:23 +0300

    Add files via upload

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Nihil";
+            int characterAge;
+            characterAge = 70;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old.");
+
+            characterName = "Nia";
+            Console.WriteLine("He reall liked the name " + characterName);
+            Console.WriteLine("But didn't like the being " + characterAge);
+
+            Console.ReadKey();
+
+        }
+    }
+}
