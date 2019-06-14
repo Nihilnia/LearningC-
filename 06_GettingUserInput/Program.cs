327c90fbbbc9ae26825a35b78b9eb5537cdbb4eb
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-15 02:05:56 +0300

    "Who is there?"

---
+++ 06_GettingUserInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_GettingUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Who is there? ");
+            string thePerson = Console.ReadLine();
+            Console.Write("Okay.. But what are you doing? ");
+            string doing = Console.ReadLine();
+            Console.Write("I hope things are going well /w " + doing + " " + thePerson);
+
+            Console.ReadKey();
+        }
+    }
+}
