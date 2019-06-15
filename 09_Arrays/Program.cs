d6963437e5ac2a93354e6c0f1b67a4ac4318118c
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-15 03:01:45 +0300

    string[ ] dearFamily = new string[3];

---
+++ 09_Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNumbers = {1, 323, 67, 55544, 110};
+            Console.WriteLine(randomNumbers[2]); //67
+            Console.WriteLine(randomNumbers[randomNumbers.Length - 1]); //Last item (110)
+
+            //Update an array
+            randomNumbers[randomNumbers.Length - 1] = 700;
+            Console.WriteLine(randomNumbers[randomNumbers.Length - 1]); //Last item 'changed' (700)
+
+            //Another way to create an array
+            string[] dearFamily = new string[3];
+            dearFamily[0] = "Necla";
+            dearFamily[1] = "Burhan";
+            dearFamily[2] = "Burak";
+            Console.WriteLine(dearFamily[2]); //Bu aralar konumuz Burak çünkü
+
+            Console.ReadKey();
+        }
+    }
+}
