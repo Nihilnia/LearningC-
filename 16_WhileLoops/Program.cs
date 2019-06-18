ccc882c491996fdf34fd115ca7d7958d834ffdf7
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-19 00:41:19 +0300

    while _ do while

---
+++ 16_WhileLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numb = 1;
+
+            while (numb <= 5)
+            {
+                Console.WriteLine(numb);
+                numb++;
+            }
+
+
+            //do while Loop
+
+            int numb2 = 8;
+
+            do
+            {
+                Console.WriteLine("\n" + numb2);
+                numb2++;
+            } while (numb2 <= 10);
+
+            Console.ReadKey();
+        }
+    }
+}
