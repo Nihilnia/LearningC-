15b29ca80b9a45571cfd519817d3764e0bf925bb
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-19 01:18:25 +0300

    for (i = 0; i <= 5; i++)

---
+++ 18_ForLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckNumbers = {2, 4, 18, 12, 24};
+
+            for (int i = 0; i < luckNumbers.Length; i++)
+            {
+                Console.WriteLine(luckNumbers[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
