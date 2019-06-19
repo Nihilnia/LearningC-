8a95070bea2a24beae74c196ba5de22f7c41f071
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-19 03:39:45 +0300

    That was cool

---
+++ 19_BuildingAnExponentMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_BuildingAnExponentMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumb, powNumb;
+            Console.Write("Base number: ");
+            baseNumb = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Power number: ");
+            powNumb = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+
+            for (int i = 1; i <= powNumb; i++)
+            {
+                result *= baseNumb;
+            }
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
