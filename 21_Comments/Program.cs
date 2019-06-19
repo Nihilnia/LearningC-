811edd2daf7234660bbe5e23d03ba6832be577d5
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-19 05:19:22 +0300

    // this is /* cool */

---
+++ 21_Comments/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //There is two types to make comment line/lines
+
+            //This one 
+
+            /*
+             * And
+             * 
+             * This
+             * 
+             * One
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
