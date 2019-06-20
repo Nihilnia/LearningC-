afeb360e22c01f59b8b3d4e1e09c533c2b323e17
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-20 15:47:22 +0300

    public string artist;

---
+++ 23_ClassesAndObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song01 = new Song();
+
+            song01.name = "Virulence";
+            song01.artist = "Helion";
+            song01.id = 123456;
+
+            Console.WriteLine(song01.id);
+            Console.WriteLine(song01.artist);
+            Console.WriteLine(song01.name);
+
+            Console.ReadKey();
+        }
+    }
+}
