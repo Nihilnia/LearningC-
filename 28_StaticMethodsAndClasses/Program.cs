da72d8dec95359e773331d72795d082cd28809b5
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-21 12:12:54 +0300

    static class & public static void SayHi( )

---
+++ 28_StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_StaticMethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsefulTools.SayHi("Nihil");
+
+            Console.ReadKey();
+        }
+    }
+}
