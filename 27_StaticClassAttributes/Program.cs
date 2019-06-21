9177dfc154a4c7ea0d14e1a248879cb012098d4e
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-21 11:56:25 +0300

    public static int songCount = 0;

---
+++ 27_StaticClassAttributes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song01 = new Song("Broken Phone Blues", "Black Strobe", 4.34);
+            song01.ShowInfos();
+
+            Song song02 = new Song("A New World", "Zülfü Livaneli,", 4.32);
+            song02.ShowInfos();
+
+            // Show the count that how many song is there?
+
+            Console.WriteLine("There is totally {0} song created!", Song.songCount);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 27_StaticClassAttributes/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_StaticClassAttributes
+{
+    class Song
+    {
+        public string name, artist;
+        public double duration;
+        public static int songCount = 0;
+
+        public Song(string pName, string pArtist, double pDuration)
+        {
+            artist = pArtist;
+            name = pName;
+            duration = pDuration;
+            songCount += 1;
+        }
+
+        public void ShowInfos()
+        {
+            Console.WriteLine("----- ALL INFORMATIONS OF SONG -----\n" +
+                "Name: {0}\n" +
+                "Artist: {1}\n" +
+                "Duration: {2}\n", name, artist, duration);
+        }
+
+        public int getSongCount()
+        {
+            return songCount;
+        }
+    }
+
+}
