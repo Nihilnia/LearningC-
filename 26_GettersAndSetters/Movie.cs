8bf3e46a3bc706cd23917858731ea9bb3ea26cb5
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-21 11:58:06 +0300

    public int Rating{ }

---
+++ 26_GettersAndSetters/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_GettersAndSetters
+{
+    class Movie
+    {
+        public string name, director;
+        private int rating;
+
+        public Movie(string pName, string pDirector, int pRating)
+        {
+            name = pName;
+            director = pDirector;
+            Rating = pRating;
+        }
+
+        public void showInfos()
+        {
+            Console.WriteLine("#### All Informations of Movie\n" +
+                "Name: {0}\n" +
+                "Director: {1}\n" +
+                "Rating: {2}\n\n", name, director, rating);
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set {
+                if (value == 1 || value == 2 || value == 3)
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = 0;
+                }
+            }
+        }
+    }
+}
---
+++ 26_GettersAndSetters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie01 = new Movie("Shrek", "ASDASD", -1);
+
+            movie01.showInfos();
+
+            Console.ReadKey();
+        }
+    }
+}
