3039693a5900ed0c4ff83c97ae37cc41af19c8c7
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-21 10:30:38 +0300

    public Song( )

---
+++ 24_Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song01 = new Song("House of the Rising Sun", "The Animals", 123456);
+
+            string artist = song01.artist;
+            Console.WriteLine("Artist: " + artist);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 24_Constructors/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Constructors
+{
+    class Song
+    {
+        public string name, artist;
+        public int id;
+
+        public Song(string songName, string songArtist, int songID)
+        {
+            name = songName;
+            artist = songArtist;
+            id = songID;
+
+            Console.WriteLine("New song created!\n" +
+                "Name: {0}\n" +
+                "Artist: {1}\n" +
+                "ID: {2}", name, artist, id);
+        }
+    }
+}
