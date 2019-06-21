b19b24d3777dc5c54def584bfecff4da10cecfc8
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-06-21 10:49:48 +0300

    Add files via upload

---
+++ 25_ObjectsMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_ObjectsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student01 = new Student("Nihil", "Nia", 3.6);
+            Student student02 = new Student("Simulation", "Simulacra", 3.4);
+
+            student01.showInfos();
+
+
+            if (student01.honorReward())
+            {
+                Console.WriteLine("Student {0} deserve to get honor reward.", student01.name);
+            }
+            else
+            {
+                Console.WriteLine("Student {0} NOT deserve to get honor reward.", student01.name);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 25_ObjectsMethods/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_ObjectsMethods
+{
+    class Student
+    {
+        public string name, surname;
+        public double gpa;
+
+        public Student(string aName, string aSurname, double aGpa)
+        {
+            name = aName;
+            surname = aSurname;
+            gpa = aGpa;
+        }
+
+        public void showInfos()
+        {
+            Console.WriteLine("- - - - Student's Informations - - - -\n" +
+                "Name: {0}\n" +
+                "Surname: {1}\n" +
+                "GPA: {2}\n\n", name, surname, gpa);
+        }
+
+        public bool honorReward()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
